6a20d44a47d756477064fb2bf9bd0941028bc885
TeaPress <teapress@takeatea.com>
2013-10-04 17:58:34 +0200

    add Array Copy

--- vendor/TakeATea/Model/AbstractModel.php
+++ vendor/TakeATea/Model/AbstractModel.php
@@ -3,7 +3,12 @@
 namespace TakeATea\Model;
 
 abstract class AbstractModel
-{    
+{
+    /**
+     * Exchange array to data
+     * @param array $data
+     * @return \TakeATea\Model\AbstractModel
+     */
     public function exchangeArray(array $data)
     {
         $attributes = get_object_vars($this);
@@ -15,9 +20,22 @@ abstract class AbstractModel
         return $this;
     }
     
+    /**
+     * Transform object to array
+     * @return array
+     */
     public function toArray()
     {
         $vars = get_object_vars($this);
         return $vars;
+    }
+    
+    /**
+     * Get copy array
+     * @return array
+     */
+    public function getArrayCopy()
+    {
+        return $this->toArray();
     }
 }
